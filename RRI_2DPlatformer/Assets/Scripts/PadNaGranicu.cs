78c40621575fa88a0bf72af554cf9ed1e870d307
KarloSever <karlo.sever@student.mev.hr>
2020-05-06 17:39:06 +0200

    2DPlatformer

---
+++ RRI_2DPlatformer/Assets/Scripts/PadNaGranicu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadNaGranicu : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Player"))
+			Application.LoadLevel (Application.loadedLevel);
+	}
+}
