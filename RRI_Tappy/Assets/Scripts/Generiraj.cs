3020f7fba6f37308535884a19c28310986145074
KarloSever <karlo.sever@student.mev.hr>
2020-05-16 13:00:36 +0200

    Tappy

---
+++ RRI_Tappy/Assets/Scripts/Generiraj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generiraj : MonoBehaviour {
+	public GameObject stijene;
+	int rezultat=0;
+	void Start () {
+		InvokeRepeating ("KreirajStijene", 1f, 1.5f);
+	}
+	void OnGUI(){
+		GUI.color = Color.black;
+		GUILayout.Label ("Rezultat: " + rezultat.ToString ());
+	}
+
+	void KreirajStijene () {
+		Instantiate (stijene);
+		rezultat++;
+	}
+}
---
+++ RRI_Tappy/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Vector2 jumpforce = new Vector2 (0, 300);
+
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D> ().AddForce (jumpforce);
+		}
+		Vector2 screenPoints = Camera.main.WorldToScreenPoint (transform.position);
+		if (screenPoints.y > Screen.height || screenPoints.y < 0) {
+			Die ();
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		Die ();
+	}
+	void Die(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
