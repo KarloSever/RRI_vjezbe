f86a611b3c4a074a4822535e7095ad900afd64e6
KarloSever <karlo.sever@student.mev.hr>
2020-07-13 15:55:36 +0200

    Završni projekt - igra Zmija

---
+++ ZAVRŠNI PROJEKT/Zmija/Assets/StvarajHranu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StvarajHranu : MonoBehaviour {
+	// hrana
+	public GameObject hranaPrefab;
+
+	// zidovi unutar kojih se zmija krece
+	public Transform BorderTop;
+	public Transform BorderBottom;
+	public Transform BorderLeft;
+	public Transform BorderRight;
+
+
+	void Start () {
+		// stvara hranu svake 4 sekunde
+		InvokeRepeating("Stvaranje", 3, 4);
+	}
+
+	// stvaranje jednog elementa hrane
+	void Stvaranje() {
+		// x pozicija od lijevog i desnog zida
+		int x = (int)Random.Range(BorderLeft.position.x,
+			BorderRight.position.x);
+
+		// y pozicija od gornjeg i donjeg zida
+		int y = (int)Random.Range(BorderBottom.position.y,
+			BorderTop.position.y);
+
+		Instantiate(hranaPrefab,
+			new Vector2(x, y),
+			Quaternion.identity); 
+	}
+}
---
+++ ZAVRŠNI PROJEKT/Zmija/Assets/Zmija.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class Zmija : MonoBehaviour {
+	// provjera da li je zmija u doticaju sa hranom
+	bool hrana = false;
+	public GameObject repPrefab;
+	// za listu na kojoj je broj elementa repa
+	List<Transform> rep = new List<Transform>();
+	// zmija se po defaultu na pocetku krece desno
+	Vector2 dir = Vector2.right;
+
+	void Start () {
+		//brzina kretanja zmije
+		InvokeRepeating("Kretanje", 0.1f, 0.1f);    
+	}
+		
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow))
+			dir = Vector2.right;
+		else if (Input.GetKey(KeyCode.DownArrow))
+			dir = -Vector2.up;    
+		else if (Input.GetKey(KeyCode.LeftArrow))
+			dir = -Vector2.right; 
+		else if (Input.GetKey(KeyCode.UpArrow))
+			dir = Vector2.up;
+	}
+
+	void Kretanje() {
+		Vector2 v = transform.position;
+		transform.Translate(dir);
+		if (hrana) {
+			// ucita se prefab
+			GameObject g =(GameObject)Instantiate(repPrefab, v, Quaternion.identity);
+			// vodi se broj repova
+			rep.Insert(0, g.transform);
+
+			hrana = false;
+		}
+
+		// provjera da li zmija ima rep
+		if (rep.Count > 0) {
+			// zadnji element repa se pomice na mjesto gdje je glava bila
+			rep.Last().position = v;
+
+			// dodaje se ispred liste repova i uklanja s kraja
+			rep.Insert(0, rep.Last());
+			rep.RemoveAt(rep.Count-1);
+		}
+}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		// ako zmija naidje na hranu
+		if (coll.name.StartsWith("hranaPrefab")) {
+			// zmija se produzi u iducem pokretu
+			hrana = true;
+
+			// ukloni se hrana kad zmija ima koliziju sa objektom
+			Destroy(coll.gameObject);
+		}
+		// igra se ponovo pokrece kod sudara sa zidom
+		else {
+			Application.LoadLevel ("Game");
+		}
+	}
+}
